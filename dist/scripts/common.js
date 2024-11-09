d2a884e1c8e218eb9808958c3a8538b26eea9192
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2024-11-10 01:02:09 +0300

    first version

---
+++ dist/scripts/common.js
@@ -0,0 +1,13 @@
+function checkUserData() {
+    const url = new URL(location.href);
+    const name = url.searchParams.get('name');
+    const lastName = url.searchParams.get('lastName');
+    const email = url.searchParams.get('email');
+
+    if (!name || !lastName || !email) {
+        location.href = 'index.html';
+    }
+}
+
+const currentData = document.querySelector('.footer-date');
+currentData.innerText = new Date().getFullYear();
---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
+
+function defaultTask(cb) {
+    return gulp.src('./src/styles/scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./dist/styles'));
+}
+
+
+exports.default = defaultTask
+
+exports.watch = function () {
+    gulp.watch('./src/**/*.scss', gulp.series('default'));
+};
