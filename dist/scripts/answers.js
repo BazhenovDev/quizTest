f7b7949f3237f358f78a97a4a715fa5aaf472ff6
Ivan Bazhenov <bazhenov.dev@yandex.ru>
2024-11-17 15:26:19 +0300

    optimization answer.js

--- dist/scripts/answers.js
+++ dist/scripts/answers.js
@@ -19,7 +19,7 @@
                     }
                     this.getUserName();
                     this.getQuizTests();
-                    this.test();
+                    this.showResult();
                 } else {
                     location.href = 'index.html';
                 }
@@ -62,10 +62,10 @@
             const name = sessionStorage.getItem('name')
             const lastName = sessionStorage.getItem('lastName')
             const email = sessionStorage.getItem('email')
-            const id = sessionStorage.getItem('id')
+            const id = sessionStorage.getItem('testId')
             const score = sessionStorage.getItem('scoreQuestions')
             const total = sessionStorage.getItem('totalQuestions')
-            location.href = 'result.html?name=' + name + '&lastName=' + lastName + '&email=' + email + '&id=' + id + '&score=' + score + '&total=' + total;
+            location.href = 'result.html?name=' + name + '&lastName=' + lastName + '&email=' + email + '&id=' + id + '&score=' + score + '&total=' + total + '&id=' + id;
         },
         generateAnswers() {
             const wrapper = document.getElementById('answers-wrapper');
@@ -115,132 +115,60 @@
             const linkResultsBack = document.querySelector('.answers__link');
             linkResultsBack.addEventListener('click', this.backResult);
         },
-        test() {
-
-
-            // for (let i = 0; i < questionOption.length; i++) {
-            //     if ((this.userQuizResult[i] === this.rightsQuizResult[i])) {
-            //
-            //         questionOption[i].classList.add('try');
-            //         questionOption[i].classList.remove('false');
-            //     } else {
-            //         questionOption[i].classList.add('false');
-            //         questionOption[i].classList.remove('try');
+        showResult() {
+            // for (let i = 0; i < sessionStorage.length; i++) {
+            //     let key = sessionStorage.key(i);
+            //     if (key.includes('questionAnswer-')) {
+            //         this.userQuizResult.push(Number(sessionStorage.getItem(key)));
+            //         this.userQuizResult.sort((a, b) => a - b);
             //     }
             // }
-            // console.log(questionOption.attributes)
-            // for (let i = 0; i < answersWrapper.length; i++) {
             //
-            // }
-
-            // for (let i = 0; i < answersWrapper.length; i++) {
-            //     let test = this.quiz.questions[i].id === Number(answersWrapper[i].getAttribute('questionid'))
-            //     // if (this.quiz.questions[i].id === Number(answersWrapper[i].getAttribute('questionid'))) {
-            //     //     console.log(1)
-            //     // }
-            //     console.log(test)
-            // }
-
+            // const questionOption = document.querySelectorAll('.answers__question-option');
+            //
             // for (let i = 0; i < questionOption.length; i++) {
-            //     if ((this.rightsQuizResult[i] === this.userQuizResult[i]) || (this.rightsQuizResult[i] === Number(questionOption[i].getAttribute('value')))) {
-            //         questionOption[i].classList.add('try');
-            //         questionOption[i].classList.remove('false');
-            //     } else {
-            //         questionOption[i].classList.add('false');
-            //         questionOption[i].classList.remove('try');
-            //     }
-            // }
-
-            // questionOption.filter(item => {
-            //      return item.value
-            //  })
-
-            // console.log(this.userQuizResult[i])
-            // if ((test === this.rightsQuizResult[i]) || (this.rightsQuizResult[i] !== undefined)) {
-            //     questionOption[i].classList.add('try');
-            //     // console.log(1)
-            // }
-
-
-            // for (let i = 0; i < questionOption.length; i++) {
-            //     console.log([i])
-            // }
-
-            // this.userQuizResult.filter(resItem => {
-            //       return this.quiz.questions.filter(question => {
-            //           return question.answers.filter(answer => {
-            //               if (answer.id === resItem) {
-            //                   const answers = answer;
-            //                   this.rightsQuizResult.filter(rightItem => {
-            //                       if (rightItem === answers.id) {
-            //                           answers.test = 'try'
-            //                           console.log(answers.id)
-            //                       } else if (answers.id !== rightItem) {
-            //                           answers.test = 'false'
-            //                       }
-            //                   });
-            //                   console.log(answers)
-            //               }
-            //           });
-            //       });
-            //   });
-
-            // this.quiz.questions.forEach(item => {
-            //     const krisa = item.answers.filter(answer => {
-            //         for (let i = 0; i < this.rightsQuizResult.length; i++) {
-            //             if (this.userQuizResult[i] === this.rightsQuizResult[i] || (Number(questionOption[i].getAttribute('value') === this.userQuizResult[i]))) {
-            //                 return answer.id === this.userQuizResult[i]
-            //             }
+            //    const userResult = this.userQuizResult.filter(item => {
+            //         if (Number(questionOption[i].getAttribute('value')) === item) {
+            //             return questionOption
             //         }
             //     });
-            //     console.log(krisa)
-            // });
+            //     const rightResult = this.rightsQuizResult.filter(item => {
+            //         if (Number(questionOption[i].getAttribute('value')) === item) {
+            //             return questionOption
+            //         }
+            //      });
             //
-            //
-
+            //     if((userResult[0] === rightResult[0]) && Number(questionOption[i].getAttribute('value')) === userResult[0]) {
+            //         questionOption[i].classList.add('try')
+            //     } else if ((userResult[0] !== rightResult[0]) && Number(questionOption[i].getAttribute('value')) !== userResult[0]) {
+            //         questionOption[i].classList.add('false')
+            //     }
+            // }
 
             for (let i = 0; i < sessionStorage.length; i++) {
-                let key = sessionStorage.key(i);
-                if (key.includes('questionAnswer-')) {
+                const key = sessionStorage.key(i);
+                if (key.startsWith(`questionAnswer-`)) {
                     this.userQuizResult.push(Number(sessionStorage.getItem(key)));
-                    this.userQuizResult.sort((a, b) => a - b);
                 }
             }
+            // Сортировка ответов пользователя для корректного отображения
+            this.userQuizResult.sort((a,b) => a - b);
 
-            console.log(sessionStorage)
-            console.log(`Верные ответы ${this.rightsQuizResult}`)
-            console.log(`Ответы юзера ${this.userQuizResult}`)
-
-            console.log(this.userQuizResult)
-            console.log(this.rightsQuizResult)
-            console.log(this.quiz)
-
-
-            const questionOption = document.querySelectorAll('.answers__question-option');
-
-            for (let i = 0; i < questionOption.length; i++) {
-               const userResult = this.userQuizResult.filter(item => {
-                    if (Number(questionOption[i].getAttribute('value')) === item) {
-                        return questionOption
-                    }
-                });
-                const rightResult = this.rightsQuizResult.filter(item => {
-                    if (Number(questionOption[i].getAttribute('value')) === item) {
-                        return questionOption
-                    }
-                 });
-
-                if((userResult[0] === rightResult[0]) && Number(questionOption[i].getAttribute('value')) === userResult[0]) {
-                    questionOption[i].classList.add('try')
-                } else if ((userResult[0] !== rightResult[0]) && Number(questionOption[i].getAttribute('value')) !== userResult[0]) {
-                    questionOption[i].classList.add('false')
+            //Получаем все элементы с классом answers__question-option
+            const questionOptions = document.querySelectorAll('.answers__question-option');
+            //Преобразуем ответы в set для быстрого доступа
+            const userAnswerSet = new Set(this.userQuizResult);
+            const correctAnswerSet = new Set(this.rightsQuizResult);
+            // Обрабатываем каждую опцию
+            questionOptions.forEach(option => {
+                const optionValue = Number(option.getAttribute('value'));
+                //Проверяем ответ
+                if(userAnswerSet.has(optionValue)) {
+                        correctAnswerSet.has(optionValue) ?
+                        option.classList.add('try') :
+                        option.classList.add('false')
                 }
-            }
-
-            // console.log(questionOption[1].getAttribute('value'))
-            // if (+questionOption[1].getAttribute('value') === 2) {
-            //     questionOption[1].classList.add('try')
-            // }
+            })
         }
     }
     Answers.init();
